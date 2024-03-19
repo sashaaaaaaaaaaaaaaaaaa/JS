bf110832e1609d96785584395931ac20ea0864af
Sascha <alexandraisayevaaa@gmail.com>
2024-03-19 10:28:54 +0100

    Nr1

---
+++ JsEx.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
+rl.on('close', () => process.exit(0));
+
+async function execute() {
+    // Euer Code kommt hier hinein
+    const pass = 'A'
+    const password = await prompt("Please enter your password: ");
+    if (password === pass){
+        console.log("YE")
+    }else {
+        console.log("NOOO")
+    }
+
+    // Ende von eurem Code
+}
+execute().catch((err) => { console.error(err); }).finally(() => rl.close());
