d07b8465154eee6380bdb6f48cd3e828f4055184
Sascha <alexandraisayevaaa@gmail.com>
2024-03-31 21:26:55 +0200

    did all

--- Schleifen/N1.js
+++ Schleifen/N1.js
@@ -6,7 +6,17 @@ rl.on('close', () => process.exit(0));
 async function execute() {
     // Euer Code kommt hier hinein
 
+    const days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+    let input;
 
+    do {
+        input = await prompt("Enter a number between 1 and 7: ");
+        if (input < 1 || input > 7) {
+            console.log("INVALID");
+        }
+    } while (input < 1 || input > 7);
+
+    console.log(days[input - 1]);
     // Ende von eurem Code
 }
 execute().catch((err) => { console.error(err); }).finally(() => rl.close());
--- Schleifen/Nr2.js
+++ Schleifen/Nr2.js
@@ -6,8 +6,15 @@ rl.on('close', () => process.exit(0));
 async function execute() {
     // Euer Code kommt hier hinein
 
+    const info = [
+        { name : "Daryl" },
+        { lastName: "Dixon"},
+        { age: 50}
+    ];
 
-
+    for (let i of info) {
+        console.log(i)
+    }
     // Ende von eurem Code
 }
 execute().catch((err) => { console.error(err); }).finally(() => rl.close());
